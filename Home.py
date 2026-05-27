0bc2ff9b344b9c198c7be0e256fad7c5e3d4ed6e
Pranjal <pranjal.quantom@gmail.com>
2026-05-27 13:02:03 +0530

    created second page

--- Home.py
+++ Home.py
@@ -4,4 +4,8 @@ st.subheader("I am learning streamlit which is a python framework")
 st.text("These are small projects/pages I made while learning different syntax")
 a = st.button("Know about your Favourite Programming Language")
 if a:
-    st.switch_page("pages/main.py")
+    st.switch_page("pages/project1.py")
+b = st.button("Know about your coding level")
+if b :
+    st.switch_page("pages/project2.py")
+    
---
+++ pages/project2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Know about your coding level")
+st.text("You probably know where you are but have you ever thought in this way?")
+
+st.text("Starting with the basic question")
