d9de1db2b315e70c7c3544a0456125f319a94855
Pranjal <pranjal.quantom@gmail.com>
2026-05-27 14:33:11 +0530

    fixing

--- Home.py
+++ Home.py
@@ -5,8 +5,7 @@ st.text("I am learning streamlit which is a python framework")
 st.subheader("These are small projects/pages I made while learning different syntax")
 st.subheader("Just click on one of these or you can also access all the pages from the sidebar")
 
-pg = st.navigation([
+st.navigation([
     st.Page("pages/project1.py", title="Know your Favourite Language", icon="1️⃣"),
     st.Page("pages/project2.py", title="My Other Project", icon="2️⃣"),
 ])
-pg.run()
