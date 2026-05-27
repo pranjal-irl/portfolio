72c8aa6e56a22f87db30a3b1a594ee09c5e044b6
Pranjal <pranjal.quantom@gmail.com>
2026-05-27 14:28:38 +0530

    fixing error

--- Home.py
+++ Home.py
@@ -5,10 +5,6 @@ st.text("I am learning streamlit which is a python framework")
 st.subheader("These are small projects/pages I made while learning different syntax")
 st.subheader("Just click on one of these or you can also access all the pages from the sidebar")
 
-
-st.page_link("pages/project1.py", label="Know your Favourite Programming Language", icon="1️⃣")
-st.page_link("pages/project2.py", label="Know your Coding Level", icon="2️⃣")
-
 pg = st.navigation([
     st.Page("pages/project1.py", title="Know your Favourite Language", icon="1️⃣"),
     st.Page("pages/project2.py", title="My Other Project", icon="2️⃣"),
