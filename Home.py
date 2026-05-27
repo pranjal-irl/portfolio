7b6149ea347398c5310e1f069c28474111f4c3f6
Pranjal <pranjal.quantom@gmail.com>
2026-05-27 14:08:36 +0530

    adding icons to pages

--- Home.py
+++ Home.py
@@ -1,11 +1,8 @@
 import streamlit as st
+
 st.title("Pages")
 st.subheader("I am learning streamlit which is a python framework")
 st.text("These are small projects/pages I made while learning different syntax")
-a = st.button("Know about your Favourite Programming Language")
-if a:
-    st.switch_page("pages/project1.py")
-b = st.button("Know about your coding level")
-if b :
-    st.switch_page("pages/project2.py")
-    
+st.page_link("Home.py", label="Home", icon="🏠")
+st.page_link("project1.py", label="Know your Favourite Programming Language", icon="1️⃣")
+st.page_link("project2.py", label="Know your Coding Level", icon="2️⃣")
--- pages/project2.py
+++ pages/project2.py
@@ -4,3 +4,20 @@ st.title("Know about your coding level")
 st.text("You probably know where you are but have you ever thought in this way?")
 
 st.text("Starting with the basic question")
+phase = 7
+
+time = st.radio("How long have you been coding?" , ["0-2 months" , "2-6 months" , "6 months - 1 year" , "1-2 years" , "2-5 years" , "5-10 years" , "10+ years"])
+if time == "0-2 months":
+    st.write("So you are a beginner like me (when I am making this)✌️")
+elif time == "2-6 months":
+    st.write("So you surely know some things now 😼")
+elif time == "6 months - 1 year":
+    st.write("Getting Better and Better! 💪")
+elif time == "1-2 years":
+    st.write("Putting the work in! 🔥")
+elif time == "2-5 years":
+    st.write("That's Tufff 🥶")
+elif time == "5-10 years":
+    st.write("You're killing it ngl! 🫶")
+elif time == "10+ years":
+    st.write("A master of work 🫡")
