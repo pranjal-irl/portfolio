d2252f0b2e46161272039ee07a19e84f8121a290
Pranjal <pranjal.quantom@gmail.com>
2026-05-30 15:27:47 +0530

    raining emojis

--- pages/project2.py
+++ pages/project2.py
@@ -77,4 +77,5 @@ if picture is not None :
         st.image(picture)
     with col3:
         st.image("Images/folkmeme.jpg")
-    
+    from streamlit_extras.let_it_rain import rain
+    rain(emoji="🤣", font_size=54, falling_speed=5, animation_length=3)
